971cf5548aae2276a2fbd3e246139652882f43f3
MIMISOU\Salim <box@slimou.net>
2019-05-26 13:08:13 +0200

    NewComputer

---
+++ HappyEuro/src/main/java/Positions/Ausgabe.java
@@ -0,0 +1,7 @@
+package Positions;
+
+public class Ausgabe extends Position {
+    public Ausgabe(String type, String position, Double amount) {
+        super(type, position, amount);
+    }
+}
---
+++ HappyEuro/src/main/java/Positions/Einnahme.java
@@ -0,0 +1,7 @@
+package Positions;
+
+public class Einnahme extends Position {
+    public Einnahme(String type, String position, Double amount) {
+        super(type, position, amount);
+    }
+}
