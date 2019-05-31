ce747fb6ddd36e59366bac70437fcd34794a524a
Salim Oussayfi <box@slimou.net>
2019-05-31 21:48:21 +0200

    autocomplete

--- HappyEuro_Dropdown/src/main/java/HappyEuro.java
+++ HappyEuro_Dropdown/src/main/java/HappyEuro.java
@@ -20,7 +20,8 @@ import java.util.List;
 public class HappyEuro implements Serializable {
     private final Logger LOG = LoggerFactory.getLogger(HappyEuro.class);
     private String projectName;
-    private String position;
+    private String positionDropdown;
+    private String positionAuto;
     private List<HE_List> positions = new ArrayList<HE_List>();
     private String selectedPosition;
     private List<Position> he_bookList = new ArrayList<Position>();
@@ -28,7 +29,6 @@ public class HappyEuro implements Serializable {
     @PostConstruct
     public void init() {
         this.projectName = "Happy Euro Dropdown";
-
         positions.add(HE_List.MIETE);
         positions.add(HE_List.STROM);
         positions.add(HE_List.INTERNET);
@@ -36,19 +36,52 @@ public class HappyEuro implements Serializable {
         positions.add(HE_List.MIMI_MIETE);
         positions.add(HE_List.STRATO);
         positions.add(HE_List.PARKPLATZ);
+        generateAutoCompletedList();
     }
 
-    public void displayLocation() {
-        LOG.info("Selected value {} ", HE_List.valueOf(position).getPosition());
+    public List<HE_List> generateAutoCompletedList() {
+        List<HE_List> he_positions = new ArrayList<HE_List>();
+        for (HE_List item : HE_List.values()) {
+            he_positions.add(item);
+        }
+//        he_positions.add(HE_List.MIETE);
+//        he_positions.add(HE_List.STROM);
+//        he_positions.add(HE_List.INTERNET);
+//        he_positions.add(HE_List.ADOBE);
+//        he_positions.add(HE_List.MIMI_MIETE);
+//        he_positions.add(HE_List.STRATO);
+//        he_positions.add(HE_List.PARKPLATZ);
+        return he_positions;
+    }
+
+    public void displayDropdownValue() {
+        LOG.info("Selected value {} ", HE_List.valueOf(positionDropdown).getPosition());
         FacesMessage msg;
-        if (position != null) {
-            bookPosition(position);
-            msg = new FacesMessage("Selected", HE_List.valueOf(position).getType().getTypeValue()
-                    + " " + HE_List.valueOf(position).getPosition()
-                    + " " + HE_List.valueOf(position).getAmount());
-            this.selectedPosition = HE_List.valueOf(position).getType().getTypeValue()
-                    + ": " + HE_List.valueOf(position).getPosition()
-                    + " " + HE_List.valueOf(position).getAmount() + " Euro";
+        if (positionDropdown != null) {
+            bookPosition(positionDropdown);
+            msg = new FacesMessage("Selected", HE_List.valueOf(positionDropdown).getType().getTypeValue()
+                    + " " + HE_List.valueOf(positionDropdown).getPosition()
+                    + " " + HE_List.valueOf(positionDropdown).getAmount());
+            this.selectedPosition = HE_List.valueOf(positionDropdown).getType().getTypeValue()
+                    + ": " + HE_List.valueOf(positionDropdown).getPosition()
+                    + " " + HE_List.valueOf(positionDropdown).getAmount() + " Euro";
+        } else {
+            msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid", "City is not selected.");
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void displayAutoselectValue() {
+        LOG.info("Selected value {} ", HE_List.valueOf(positionAuto).getPosition());
+        FacesMessage msg;
+        if (positionAuto != null) {
+            bookPosition(positionAuto);
+            msg = new FacesMessage("Selected", HE_List.valueOf(positionAuto).getType().getTypeValue()
+                    + " " + HE_List.valueOf(positionAuto).getPosition()
+                    + " " + HE_List.valueOf(positionAuto).getAmount());
+            this.selectedPosition = HE_List.valueOf(positionAuto).getType().getTypeValue()
+                    + ": " + HE_List.valueOf(positionAuto).getPosition()
+                    + " " + HE_List.valueOf(positionAuto).getAmount() + " Euro";
         } else {
             msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid", "City is not selected.");
         }
@@ -62,16 +95,26 @@ public class HappyEuro implements Serializable {
         he_bookList.add(bookedPosition);
     }
 
+    /***********************************************************************/
+
     public String getProjectName() {
         return projectName;
     }
 
-    public String getPosition() {
-        return position;
+    public String getPositionAuto() {
+        return positionAuto;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
+    public void setPositionAuto(String positionAuto) {
+        this.positionAuto = positionAuto;
+    }
+
+    public String getPositionDropdown() {
+        return positionDropdown;
+    }
+
+    public void setPositionDropdown(String positionDropdown) {
+        this.positionDropdown = positionDropdown;
     }
 
     public List<HE_List> getPositions() {
