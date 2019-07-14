c71bdbfbdc710a7bad594edb48a4ef61a1bddec9
Salim Oussayfi <box@slimou.net>
2019-07-14 09:29:44 +0200

    added fixed data as enums

---
+++ HappyEuro4/src/main/java/he/FIXED_DATA/HE_List.java
@@ -0,0 +1,43 @@
+package he.FIXED_DATA;
+
+public enum HE_List {
+    MIETE(HE_Type.OUT, "Miete",800.00),
+    STROM(HE_Type.OUT, "Strom",65.00),
+    INTERNET(HE_Type.OUT, "Internet",39.95),
+    STRATO(HE_Type.OUT, "Strato",9.95),
+    ADOBE(HE_Type.OUT, "Adobe",29.95),
+    LOTTERIE(HE_Type.OUT, "Fernseh Lotterie",20.00),
+    HANDY(HE_Type.OUT, "Handy",20.00),
+    UNITY(HE_Type.OUT, "Unity Media",39.95),
+    GEZ(HE_Type.OUT, "GEZ",52.50),
+    FAHRKARTE(HE_Type.OUT, "Fahrkarte",92.50),
+    RECHTSCHUTZ(HE_Type.OUT, "Rechtschutz",23.72),
+    HAFTPFLICHT(HE_Type.OUT, "Haftpflicht",10.00),
+    PARKPLATZ(HE_Type.IN, "Parkplatz",50.00),
+    MIMI_MIETE(HE_Type.IN, "Mimi Miete",325.00),
+    OUTPUT(HE_Type.OUT, null,0.0),
+    INTAKE(HE_Type.IN, null,0.0),
+    BANK(HE_Type.BANK, null,0.0);
+
+    private final String position;
+    private final double amount;
+    private final HE_Type type;
+
+    HE_List(HE_Type type, String position, double amount) {
+        this.type = type;
+        this.position = position;
+        this.amount = amount;
+    }
+
+    public HE_Type getType() {
+        return type;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+}
---
+++ HappyEuro4/src/main/java/he/FIXED_DATA/HE_Type.java
@@ -0,0 +1,21 @@
+package he.FIXED_DATA;
+
+/**
+ * User: salim
+ * Date: 31.05.2019 11:08
+ */
+public enum HE_Type {
+    OUT("Ausgabe"),
+    IN("Einnahme"),
+    BANK("Guthaben");
+
+    private final String typeValue;
+
+    HE_Type(String typeValue) {
+        this.typeValue = typeValue;
+    }
+
+    public String getTypeValue() {
+        return typeValue;
+    }
+}
